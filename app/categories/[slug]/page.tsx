11c0a5c18d879a0ab1331bdf1b3988ec9cfc050d
Alexander Blomberg <info@dalasoftware.se>
2023-12-15 21:37:06 +0100

    Add the Edit Button

--- app/categories/[slug]/page.tsx
+++ app/categories/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import prisma from '@/prisma/client';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import React from 'react';
 
@@ -13,9 +14,16 @@ const CategoryDetailPage = async ({ params }: Props) => {
   if (!category) notFound();
 
   return (
-    <div>
-      <h1>{category.title} posts</h1>
-      <p>{category.slug}</p>
+    <div className='flex flex-row'>
+      <div className='basis-3/4'>
+        <h1>{category.title} posts</h1>
+        <p>{category.slug}</p>
+      </div>
+      <div className='basis-1/4'>
+        <button className='btn btn-primary'>
+          <Link href={`/categories/${category.slug}/edit`}>Edit</Link>
+        </button>
+      </div>
     </div>
   );
 };
