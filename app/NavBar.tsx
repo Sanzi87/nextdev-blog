6bfc6c02dd2285bf5f2d51b2fb690a914fe8e39f
Alexander Blomberg <info@dalasoftware.se>
2023-12-16 17:30:47 +0100

    Add the Log in and Log out links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,14 +4,15 @@ import { usePathname } from 'next/navigation';
 import React from 'react';
 import { AiFillBug } from 'react-icons/ai';
 import classnames from 'classnames';
+import { useSession } from 'next-auth/react';
 
 const NavBar = () => {
   const currentPath = usePathname();
+  const { status, data: session } = useSession();
   const links = [
     { label: 'Home', href: '/' },
     { label: 'Posts', href: '/posts' },
     { label: 'Categories', href: '/categories' },
-    { label: 'Login', href: '/login' },
   ];
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
@@ -20,18 +21,27 @@ const NavBar = () => {
       </Link>
       <ul className='navbar-links flex space-x-6'>
         {links.map((link) => (
-          <Link
-            key={link.href}
-            className={classnames({
-              'active-link': link.href === currentPath,
-              'transition-colors': true,
-            })}
-            href={link.href}
-          >
-            {link.label}
-          </Link>
+          <li key={link.href}>
+            <Link
+              className={classnames({
+                'active-link': link.href === currentPath,
+                'transition-colors': true,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
+      <div>
+        {status === 'authenticated' && (
+          <Link href='/api/auth/signout'>Log out</Link>
+        )}
+        {status === 'unauthenticated' && (
+          <Link href='/api/auth/signin'>Log in</Link>
+        )}
+      </div>
     </nav>
   );
 };
