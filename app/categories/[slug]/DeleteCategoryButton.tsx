9dee37441473760b87df9bd9770acc1dda499360
Alexander Blomberg <info@dalasoftware.se>
2023-12-16 11:38:27 +0100

    Show a confirmation dialog box

--- app/categories/[slug]/DeleteCategoryButton.tsx
+++ app/categories/[slug]/DeleteCategoryButton.tsx
@@ -1,11 +1,37 @@
+'use client';
 import Link from 'next/link';
 import React from 'react';
 
 const DeleteCategoryButton = ({ categorySlug }: { categorySlug: string }) => {
   return (
-    <button className='btn btn-error'>
-      <Link href={`/categories/${categorySlug}/edit`}>Delete</Link>
-    </button>
+    <>
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+      <button
+        className='btn btn-error'
+        onClick={() => document.getElementById('my_modal_5').showModal()}
+      >
+        {/* <Link href={`/categories/${categorySlug}/edit`}>Delete</Link> */}{' '}
+        Delete
+      </button>
+      <dialog id='my_modal_5' className='modal modal-bottom sm:modal-middle'>
+        <div className='modal-box'>
+          <h3 className='font-bold text-lg'>Confirm Deletion</h3>
+          <p className='py-4'>
+            Are you sure you want to delete this category? This action cannot be
+            undone.
+          </p>
+          <div className='modal-action'>
+            <form method='dialog'>
+              {/* if there is a button in form, it will close the modal */}
+              <button className='btn btn-outline'>Cancel</button>
+              <button className='btn btn-error ml-2'>
+                <Link href={`/categories/${categorySlug}/edit`}>Delete</Link>
+              </button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    </>
   );
 };
 
