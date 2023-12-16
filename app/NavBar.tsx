6fad2bfdec4632f0cc3d1902928d7603f09a5651
Alexander Blomberg <info@dalasoftware.se>
2023-12-16 18:11:06 +0100

    Change the layout of the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -15,32 +15,40 @@ const NavBar = () => {
     { label: 'Categories', href: '/categories' },
   ];
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-      <Link href='/'>
-        <AiFillBug />
-      </Link>
-      <ul className='navbar-links flex space-x-6'>
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              className={classnames({
-                'active-link': link.href === currentPath,
-                'transition-colors': true,
-              })}
-              href={link.href}
-            >
-              {link.label}
+    <nav className='space-x-6 border-b mb-5 px-5 py-3'>
+      <div className='container mx-auto'>
+        <div className='flex justify-between'>
+          <div className='flex items-center gap-3'>
+            <Link href='/'>
+              <AiFillBug />
             </Link>
-          </li>
-        ))}
-      </ul>
-      <div>
-        {status === 'authenticated' && (
-          <Link href='/api/auth/signout'>Log out</Link>
-        )}
-        {status === 'unauthenticated' && (
-          <Link href='/api/auth/signin'>Log in</Link>
-        )}
+          </div>
+          <div>
+            <ul className='navbar-links flex space-x-6'>
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    className={classnames({
+                      'active-link': link.href === currentPath,
+                      'transition-colors': true,
+                    })}
+                    href={link.href}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            {status === 'authenticated' && (
+              <Link href='/api/auth/signout'>Log out</Link>
+            )}
+            {status === 'unauthenticated' && (
+              <Link href='/api/auth/signin'>Log in</Link>
+            )}
+          </div>
+        </div>
       </div>
     </nav>
   );
